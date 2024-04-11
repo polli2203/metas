3494fe7d6e544d01c267702a70dac283697b73ec
polli2203 <146850815+polli2203@users.noreply.github.com>
2024-04-11 08:31:34 -0300

    jh

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-10T00:00:00");
 const tempoObjetivo3 = new Date("2024-11-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
